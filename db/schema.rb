347aa9df043ba622e5582a0f473ff697a76a3513
Ommi Shimizu <oshimizu15@gmail.com>
2017-10-24 18:07:57 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171023223343) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "session_token", null: false
