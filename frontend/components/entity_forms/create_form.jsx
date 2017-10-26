519efbbd93ecbe5f33828cb3ef72116caf25d365
Ommi Shimizu <oshimizu15@gmail.com>
2017-10-25 23:14:17 -0400

    List notebook errors in create form

--- frontend/components/entity_forms/create_form.jsx
+++ frontend/components/entity_forms/create_form.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { merge } from 'lodash';
 
 class CreateForm extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {title: ""};
+    this.state = this.props;
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(e) {
-    this.props.createItem(this.state);
-    this.props.toggleCreateForm();
+    this.props.createItem(this.state.item);
+    const blankItem = {item: { title: "" }};
+    const newState = merge(this.state, blankItem);
+    // Form submission canceled because the form is not connected
+    // error in Chrome, any significance?
   }
 
   handleCancel(e) {
     e.preventDefault();
+    const blankItem = {item: { title: "" }};
+    const newState = merge(this.state, blankItem);
+    this.setState(newState);
     this.props.toggleCreateForm();
   }
 
+  handleChange (e) {
+    const newItem = {item: { title: e.target.value } };
+    let newState = merge(this.state, newItem);
+    this.setState(newState);
+  }
+
+  componentWillReceiveProps (newProps) {
+    if (newProps.createFormOpen && newProps.errors.length > 0) {
+      this.setState(newProps);
+    }
+    else if (!newProps.createFormOpen && newProps.errors.length > 0) {
+      const blankItem = {item: { title: "" }, errors: []};
+      const newState = merge(this.state, blankItem);
+      this.setState(newState);
+    }
+    // TODO figure out how to clear errors!
+  }
+
   render() {
+    let errors = this.state.errors || [];
+    errors = errors.map((error, i) => <li key={i}>{error}</li>);
     return (
       <Modal
         isOpen={this.props.createFormOpen}
@@ -32,12 +60,16 @@ class CreateForm extends React.Component {
           <input
             type="text"
             placeholder={this.props.formMessage}
-            value={this.state.title}/>
+            value={this.state.title}
+            onChange={this.handleChange}/>
           <button onClick={this.handleCancel}
-            className="square-button" >Cancel</button>
+            className="square-button grey-button" >Cancel</button>
           <input type="submit" value={this.props.buttonMessage}
             className="square-button" />
         </form>
+        <ul>
+          {errors}
+        </ul>
       </Modal>
     );
   }
--- frontend/components/entity_forms/create_form_container.js
+++ frontend/components/entity_forms/create_form_container.js
@@ -7,20 +7,24 @@ const mapStateToProps = (state, ownProps) => {
   let formMessage;
   let buttonMessage;
   let createFormOpen = state.ui.createForm;
+  let errors;
+  let item;
   if (ownProps.itemType === "notebook") {
     formMessage = "Title your notebook";
     buttonMessage = "Create notebook";
+    errors = state.errors.notebookErrors;
+    item = { title: "" };
   } else {
     formMessage = "Name your tag";
     buttonMessage = "Create tag";
   }
-
   return {
     formMessage,
     buttonMessage,
     createFormOpen,
+    item,
+    errors,
   };
-
 };
 
 
