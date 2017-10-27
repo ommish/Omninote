6b7d919fa9701f6c08bc2b6dcfc7ce4f00213e7e
Ommi Shimizu <oshimizu15@gmail.com>
2017-10-27 12:30:54 -0400

    Styling to notes index

--- frontend/components/note_index/note_index_item.jsx
+++ frontend/components/note_index/note_index_item.jsx
@@ -5,14 +5,20 @@ class NoteIndexItem extends React.Component {
 
   render () {
     return (
-      <li className="note-index-item">
-          <ul>
-            <li className="note-index-item-title">{this.props.note.title}</li>
-            <li><p>{this.props.note.updatedAt}</p></li>
-            <li><p>{this.props.bodySnippet}</p></li>
-          </ul>
-      </li>
-    );
+      <NavLink to={
+          this.props.match.params.notebookId ?
+          `/notebooks/${this.props.match.params.notebookId}/notes/${this.props.note.id}` :
+          `/notes/${this.props.note.id}`}
+          activeClassName="active-note-item" >
+          <div className="note-item">
+            <ul>
+              <li className="note-item-title">{this.props.note.title}</li>
+              <li className="note-item-date-"><p>{new Date(this.props.note.updatedAt).dateString}</p></li>
+              <li className="note-item-body-snippet"><p>{this.props.bodySnippet}</p></li>
+            </ul>
+          </div>
+        </NavLink>
+      );
+    }
   }
-}
-export default NoteIndexItem;
+  export default NoteIndexItem;
