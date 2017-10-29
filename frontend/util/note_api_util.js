7780cbe228f40f582fafa4ccd55bb5cc8b11b367
Ommi Shimizu <oshimizu15@gmail.com>
2017-10-29 15:26:35 -0400

    Convert note params to snake_case before request

--- frontend/util/note_api_util.js
+++ frontend/util/note_api_util.js
@@ -1,3 +1,5 @@
+var snakeCase = require('snake-case');
+
 export const fetchNotes = () => {
   return $.ajax({
     url: 'api/notes',
@@ -13,18 +15,22 @@ export const fetchNote = (noteId) => {
 };
 
 export const createNote = (note) => {
+  const snakeCaseNote = {};
+  Object.keys(note).forEach((noteParam) => {snakeCaseNote[snakeCase(noteParam)] = note[noteParam];});
   return $.ajax({
     url: `api/notes`,
     method: 'post',
-    data: { note }
+    data: { note: snakeCaseNote }
   });
 };
 
 export const updateNote = (note) => {
+  const snakeCaseNote = {};
+  Object.keys(note).forEach((noteParam) => {snakeCaseNote[snakeCase(noteParam)] = note[noteParam];});
   return $.ajax({
     url: `api/notes/${note.id}`,
     method: 'patch',
-    data: { note }
+    data: { note: snakeCaseNote }
   });
 };
 
