ff74c2045c9b82293affca1badc84bcbba6fc943
Ommi Shimizu <oshimizu15@gmail.com>
2017-11-01 14:49:50 -0400

    Edit production env for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,8 @@ Rails.application.configure do
       :bucket => ENV["s3_bucket"],
       :access_key_id => ENV["s3_access_key_id"],
       :secret_access_key => ENV["s3_secret_access_key"],
-      :s3_region => ENV["s3_region"]
+      :s3_region => ENV["s3_region"],
+      :s3_host_name => ENV["s3_region"].amazonaws.com
     }
   }
 end
