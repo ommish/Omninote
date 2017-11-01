1034aa8b7f53bf4dcbee282832d6959990b0ad40
Ommi Shimizu <oshimizu15@gmail.com>
2017-11-01 12:17:07 -0400

    Configure image url protocol

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, default_url: "missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, s3_protocol: :https
+  has_attached_file :image, default_url: "missing.png", s3_protocol: :https
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :user
 end
