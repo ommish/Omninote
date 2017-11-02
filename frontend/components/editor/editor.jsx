74e0fb49e63e97f8f9712bc702e8297554788f53
Ommi Shimizu <oshimizu15@gmail.com>
2017-11-02 12:23:22 -0400

    Update how editor responds to new props

--- frontend/components/editor/editor.jsx
+++ frontend/components/editor/editor.jsx
@@ -5,7 +5,6 @@ import Modal from 'react-modal';
 import NotebookDropdown from './notebook_dropdown_container';
 import { quillModules, quillFormats } from '../../util/quill_configs';
 
-
 class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -28,18 +27,27 @@ class Editor extends React.Component {
 
     this.quillEditor = null;
 
-    this.attemptSave = this.attemptSave.bind(this);
+    // this.attemptSave = this.attemptSave.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
-    if (this.props.location.pathname !== newProps.location.pathname) {
-      this.attemptSave();
-      this.setState(newProps);
-      if (this.props.tagErrors.length > 0 || this.props.noteErrors.length > 0) {
+    if (this.props.location.pathname !== newProps.location.pathname && newProps.note.id) {
+      if (this.state.note.title !== "" && this.props.selectedNotebook.id) {
+        const newState = merge({}, this.state);
+        newState.note.notebookId = this.props.selectedNotebook.id;
+        this.props.action(this.state.note).then(() => {
+          this.props.clearTagErrors();
+          this.props.clearNoteErrors();
+          this.setState(newProps);
+        });
+      } else {
         this.props.clearTagErrors();
         this.props.clearNoteErrors();
+        this.setState(newProps);
       }
     } else if (this.props.note.tagIds.length !== newProps.note.tagIds.length) {
+      this.props.clearTagErrors();
+      this.props.clearNoteErrors();
       this.setState(newProps);
     }
   }
@@ -80,36 +88,28 @@ class Editor extends React.Component {
   }
 
   handleBodyChange (content, delta, source, editor) {
-    const newState = merge({}, this.state);
-    const newNote = merge(newState.note, {body: content, bodyPlain: editor.getText().trim()});
-    newState.note = newNote;
+    let newState = merge({}, this.state);
+    newState.note = merge(newState.note, {body: content, bodyPlain: editor.getText().trim()});
     this.setState(newState);
-    if (this.state.title !== "" && !this.state.saved && this.props.selectedNotebook.id) {
-      this.attemptSave();
-    }
-  }
-
-  attemptSave() {
-    const newState = merge({}, this.state);
-    newState.note.notebookId = this.props.selectedNotebook.id;
-    newState.saved = true;
-    this.setState(newState);
-    this.props.action(newState.note).then((success) => {
-      if (!this.props.match.params.noteId) {
-        if (this.props.match.params.notebookId) {
-          this.props.history.push(`/notebooks/${success.note.notebookId}/notes/${success.note.id}`);
-        } else {
-          this.props.history.push(`/notes/${success.note.id}`);
+    if (this.state.note.title !== "" && this.props.selectedNotebook.id) {
+      newState = merge({}, this.state);
+      newState.note.notebookId = this.props.selectedNotebook.id;
+      this.setState(newState);
+      this.props.action(newState.note).then((success) => {
+        if (!this.props.match.params.noteId) {
+          if (this.props.match.params.notebookId) {
+            this.props.history.push(`/notebooks/${success.note.notebookId}/notes/${success.note.id}`);
+          } else {
+            this.props.history.push(`/notes/${success.note.id}`);
+          }
         }
-      }
-    },
-    (fail) => {
-      this.props.clearTagErrors();
-      this.props.clearNoteErrors();
+      },
+      (fail) => {
+        this.props.clearTagErrors();
+        this.props.clearNoteErrors();
+      });
     }
-  );
   }
-
 
   handleTitleChange(e) {
     const newState = merge({}, this.state);
@@ -158,84 +158,83 @@ class Editor extends React.Component {
     this.props.action(newState.note).then((success) => {
       if (this.props.fullEditor) {
         this.props.toggleFullEditor();
-        }
+      }
       this.props.history.push(`/notebooks/${success.note.notebookId}/notes/${success.note.id}`);
       this.props.clearNoteErrors();
       this.props.clearTagErrors();
     });
   }
 
-render() {
-  const tags = this.props.allTags.map((tag) => {
-    return (
-      <button
-        key={tag.id}
-        onClick={this.handleTagClick(tag.id)}
-        className={this.state.note.tagIds.includes(tag.id) ? "tag-button selected" : "tag-button"}>
-        {tag.title}
-      </button>
-    );
-  });
-
-  const noteErrors = this.props.noteErrors.map((err) => <li key={err}>{err}</li>);
-  const tagErrors = this.props.tagErrors.map((err) => <li key={err}>{err}</li>);
-
-  return (
-    <main
-      className={this.props.fullEditor ?
-        "note-editor full" :
-        "note-editor"}>
-        <div className="editor-heading">
-          <NotebookDropdown />
-          Tags: {tags}
-          <input type="text"
-            placeholder="Create new tag"
-            onKeyPress={this.createTag}
-            onChange={this.handleTagInput}
-            value={this.state.tagInput}/>
-          <ul className="editor-errors">{tagErrors}</ul>
-        </div>
-        <div className="editor-lower-heading">
-          <input
-            onChange={this.handleTitleChange}
-            placeholder="Title your note"
-            type="text"
-            className="title"
-            value={this.state.note.title}/>
-          <ul className="editor-errors">{noteErrors}</ul>
-          <div className="editor-buttons">
-            <button
-              disabled={this.state.note.title === "" ? true : false}
-              className={this.state.note.title === "" ? "square-button small narrow disabled" : "square-button small narrow"}
-              onClick={this.handleSubmit}>Save</button>
-            <button
-              className={"square-button small narrow expand"}
-              onClick={this.props.toggleFullEditor}>
-              {this.props.fullEditor ? "Close" : <img
-                className="sidenav-icon"
-                src={window.staticAssets.white_expand}>
-              </img>}
-            </button>
-          </div>
-        </div>
-        <label>Add an image:&nbsp;&nbsp;&nbsp;
-          <input type="file"
-            onChange={this.handleImage}
-            accept=".png, .gif, .jpg, .jpeg"/>
-        </label>
-        <ReactQuill
-          ref={(input) => { this.quillEditor = input; }}
-          id="quill"
-          className={this.props.fullEditor ?
-            "note-editor-quill full " :
-            "note-editor-quill"}
-            modules={quillModules}
-            formats={quillFormats}
-            value={this.state.note.body}
-            onChange={this.handleBodyChange}/>
-        </main>
+  render() {
+    const tags = this.props.allTags.map((tag) => {
+      return (
+        <button
+          key={tag.id}
+          onClick={this.handleTagClick(tag.id)}
+          className={this.state.note.tagIds.includes(tag.id) ? "tag-button selected" : "tag-button"}>
+          {tag.title}
+        </button>
       );
-    }
-  }
+    });
+    const noteErrors = this.props.noteErrors.map((err) => <li key={err}>{err}</li>);
+    const tagErrors = this.props.tagErrors.map((err) => <li key={err}>{err}</li>);
 
-  export default Editor;
+    return (
+      <main
+        className={this.props.fullEditor ?
+          "note-editor full" :
+          "note-editor"}>
+          <div className="editor-heading">
+            <NotebookDropdown />
+            Tags: {tags}
+            <input type="text"
+              placeholder="Create new tag"
+              onKeyPress={this.createTag}
+              onChange={this.handleTagInput}
+              value={this.state.tagInput}/>
+            <ul className="editor-errors">{tagErrors}</ul>
+          </div>
+          <div className="editor-lower-heading">
+            <input
+              onChange={this.handleTitleChange}
+              placeholder="Title your note"
+              type="text"
+              className="title"
+              value={this.state.note.title}/>
+            <ul className="editor-errors">{noteErrors}</ul>
+            <div className="editor-buttons">
+              <button
+                disabled={this.state.note.title === "" ? true : false}
+                className={this.state.note.title === "" ? "square-button small narrow disabled" : "square-button small narrow"}
+                onClick={this.handleSubmit}>Save</button>
+              <button
+                className={"square-button small narrow expand"}
+                onClick={this.props.toggleFullEditor}>
+                {this.props.fullEditor ? "Close" : <img
+                  className="sidenav-icon"
+                  src={window.staticAssets.white_expand}>
+                </img>}
+              </button>
+            </div>
+          </div>
+          <label>Add an image:&nbsp;&nbsp;&nbsp;
+            <input type="file"
+              onChange={this.handleImage}
+              accept=".png, .gif, .jpg, .jpeg"/>
+          </label>
+          <ReactQuill
+            ref={(input) => { this.quillEditor = input; }}
+            id="quill"
+            className={this.props.fullEditor ?
+              "note-editor-quill full " :
+              "note-editor-quill"}
+              modules={quillModules}
+              formats={quillFormats}
+              value={this.state.note.body}
+              onChange={this.handleBodyChange}/>
+          </main>
+        );
+      }
+    }
+
+    export default Editor;
--- frontend/components/note_index/note_index_item_container.js
+++ frontend/components/note_index/note_index_item_container.js
@@ -13,7 +13,6 @@ const mapStateToProps = (state, ownProps) => {
   return {
     notebook,
     bodySnippet,
-    noteErrors: state.errors.noteErrors,
   };
 };
 
