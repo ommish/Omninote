aa0bfaa34a977d3fd9f67a2fa624968b253d0511
Ommi Shimizu <oshimizu15@gmail.com>
2017-12-23 00:14:43 -0500

    Edit plugins for node env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,25 @@
 
 const path = require('path');
+const webpack = require('webpack');
+var plugins = [];
+const devPlugins = [];
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+    },
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true,
+    },
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -8,6 +28,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
