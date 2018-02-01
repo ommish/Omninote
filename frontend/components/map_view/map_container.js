4ddbac9bb7d3030a9738c4f3ebc7f90d634fe369
Ommi Shimizu <oshimizu15@gmail.com>
2018-01-31 20:04:08 -0500

    Remove import from deleted file

--- frontend/components/map_view/map_container.js
+++ frontend/components/map_view/map_container.js
@@ -1,7 +1,6 @@
 import Map from './map';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { flagsWithNotes } from '../../util/filter_util';
 import { receiveFlags } from '../../actions/flag_actions';
 
 const mapStateToProps = (state, ownProps) => {
